a2c8145613985fe9effbd0034b2427331162b179
duncanimi <duncantomke1@gmail.com>
2025-04-23 11:55:16 +0200

    layout.svelte update

---
+++ src/routes/levels/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    let {children} = $props();
+</script>
+
+<nav>
+    <a href="/"> home </a>
+    <a href= "/levels/level1"> level1</a>
+</nav>
+<h1> Das ist Level x</h1>
+<p> Das  </p>
+<input 
+type ="text"
+id = "name"
+name = "name"
+size = "200" 
+minlength="40"
+maxlength="100"/>
+
+{@render children()}
