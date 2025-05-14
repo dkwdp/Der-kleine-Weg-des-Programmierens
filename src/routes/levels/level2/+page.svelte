817040fc8e68ba7d5fb36aa924a744309a851440
duncanimi <duncantomke1@gmail.com>
2025-05-14 12:44:20 +0200

    Pythonfix2

--- src/routes/levels/level2/+page.svelte
+++ src/routes/levels/level2/+page.svelte
@@ -1,7 +1,10 @@
 <script>
+   import { onMount } from 'svelte';
   import { myVariable } from '$lib/stores/editorStore';
 
-  myVariable.set("#funktioniierreeeee biiitteee2222"); // oder abhängig vom Level  
+   onMount(() => {
+  myVariable.set("#funktioniierreeeee biiitteee2222");
+    }); // oder abhängig vom Level  
 </script>
 <h1>Level2</h1>
 <p>Aufgabe 2:</p>
