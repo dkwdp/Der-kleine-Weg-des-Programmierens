f449cc7d66be60017eb21984cf59398366e57c29
annadeus <166652253+annadeus@users.noreply.github.com>
2025-06-11 12:42:04 +0200

    removed HEAD out of editorStore.ts file

--- src/lib/stores/editorStore.ts
+++ src/lib/stores/editorStore.ts
@@ -3,12 +3,8 @@ import { writable } from 'svelte/store';
 export const myVariable = writable("# Initial code");
 export const pythonCode = writable("");
 export const pythonOutput =  writable("");
-<<<<<<< HEAD
-export const isCurrentLevelDrawing = writable(false);
-=======
 export const isCurrentLevelDrawing = writable(false);
 export const solvedLevel = writable(false);
 export const levelID = writable(0);
 export const outputID = writable(0);
  
->>>>>>> newLevelOle
